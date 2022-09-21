464eec059dc5688b3a2129b28b53b18719633f05
HLABZ <113842155+HLABZcc@users.noreply.github.com>
2022-09-21 03:38:27 +0200

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -129,21 +129,6 @@ export const footerLinks = [
       },
     ],
   },
-    {
-    title: "⭐️ Community",
-    links: [
-      {
-        name: "GitHub",
-        link: "https://github.com/HLABZcc",
-        url: "https://github.com/HLABZcc",
-      },
-      {
-        name: "Fiverr",
-        link: "https://www.fiverr.com/hlabzconsult",
-        url: "https://www.fiverr.com/hlabzconsult",
-      },
-    ],
-  },
   {
     title: "🤝 Sponsors",
     links: [
@@ -172,6 +157,21 @@ export const footerLinks = [
         link: "https://www.bruleur2graisse.xyz/",
         url: "https://www.bruleur2graisse.xyz/",
       },
+          {
+    title: "⭐️ Community",
+    links: [
+      {
+        name: "GitHub",
+        link: "https://github.com/HLABZcc",
+        url: "https://github.com/HLABZcc",
+      },
+      {
+        name: "Fiverr",
+        link: "https://www.fiverr.com/hlabzconsult",
+        url: "https://www.fiverr.com/hlabzconsult",
+      },
+    ],
+  },
     ],
   },
 ];
