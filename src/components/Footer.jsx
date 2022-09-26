edbb51787fd638ed37bd8ad91840cec8bbc35e8b
HLABZ <113842155+HLABZcc@users.noreply.github.com>
2022-09-26 09:02:29 +0200

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -42,11 +42,6 @@ const Footer = () => (
       <p className="font-poppins font-normal text-center text-[18px] leading-[27px] text-white">
         Copyright Ⓒ 2022 | <a href="https://hlabz.cc/" alt="HLABZ.cc®">HLABZ.cc®</a> - All Rights Reserved.
       </p>
-      <p className="font-poppins font-normal text-center text-sm text-white">
-          <p>DONATE</p>
-          <p>BTC: bc1qp4zxxj0k0whkm483qv5dk2rs6sgh2tt70h44rf</p>
-          <p>LTC: ltc1qtyqsh3sgnaa9sdhe89p6qu2s6g75s70yzeqdyu</p>
-      </p>
 
       <div className="flex flex-row md:mt-0 mt-6">
         {socialMedia.map((social, index) => (
@@ -60,7 +55,7 @@ const Footer = () => (
           />
         ))}
       </div>
-            <p className="font-poppins font-normal text-center text-sm text-white">
+          <p className="font-poppins font-normal text-center text-sm text-white">
           <p>DONATE</p>
           <p>BTC: bc1qp4zxxj0k0whkm483qv5dk2rs6sgh2tt70h44rf</p>
           <p>LTC: ltc1qtyqsh3sgnaa9sdhe89p6qu2s6g75s70yzeqdyu</p>
