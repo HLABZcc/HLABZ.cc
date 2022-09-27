ffb2a30aa879851c9648833cf348ab8632eba7ce
HLABZ <113842155+HLABZcc@users.noreply.github.com>
2022-09-27 23:57:26 +0200

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,7 +15,8 @@ const Hero = () => {
           </p></center>
         </div>
 
-        <center><div className="">
+        <center><div className=""><blockquote class="twitter-tweet"><p lang="en" dir="ltr">Let’s Boost your <a href="https://twitter.com/hashtag/SEO?src=hash&amp;ref_src=twsrc%5Etfw">#SEO</a> Results! 🚀<br><br>Whether you manage a <a href="https://twitter.com/hashtag/blog?src=hash&amp;ref_src=twsrc%5Etfw">#blog</a>, an <a href="https://twitter.com/hashtag/ecommerce?src=hash&amp;ref_src=twsrc%5Etfw">#ecommerce</a> website or a <a href="https://twitter.com/hashtag/SAAS?src=hash&amp;ref_src=twsrc%5Etfw">#SAAS</a>, <a href="https://twitter.com/hashtag/HLABZ?src=hash&amp;ref_src=twsrc%5Etfw">#HLABZ</a>® will allow you to plan and manage the growth of your <a href="https://twitter.com/hashtag/business?src=hash&amp;ref_src=twsrc%5Etfw">#business</a> easily!<br><br>🛠 Work Together : <a href="https://t.co/usbtkkt21X">https://t.co/usbtkkt21X</a><br>🌐 Website : <a href="https://t.co/wW5WGjBX5Q">https://t.co/wW5WGjBX5Q</a> <a href="https://t.co/efipVppJbV">pic.twitter.com/efipVppJbV</a></p>&mdash; HLABZ.cc (@HLABZcc) <a href="https://twitter.com/HLABZcc/status/1573732760711307269?ref_src=twsrc%5Etfw">September 24, 2022</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+        
           <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100.8px] leading-[75px]">
             Welcome to <br></br>
           </h1>
