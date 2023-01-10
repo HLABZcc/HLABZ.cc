8c65e9ff3cd5b04cd4a3e850ad9d1104f5bdba59
HLABZ® <113842155+HLABZcc@users.noreply.github.com>
2023-01-10 15:28:37 +0100

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -40,7 +40,7 @@ const Footer = () => (
 
     <div className="w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3F3E45]">
       <p className="font-poppins font-normal text-center text-[18px] leading-[27px] text-white">
-        Copyright Ⓒ 2022 | <a href="https://hlabz.cc/" alt="HLABZ.cc®">HLABZ.cc®</a> - All Rights Reserved.
+        Copyright Ⓒ 2023 | <a href="https://hlabz.cc/" alt="HLABZ.cc®">HLABZ.cc®</a> - All Rights Reserved.
       </p>
 
       <div className="flex flex-row md:mt-0 mt-6">
@@ -57,7 +57,7 @@ const Footer = () => (
       </div>
       <br />
           <p className="font-poppins font-normal text-center text-sm text-white">
-          <strong><p>DONATE (₿)</p></strong>
+          <strong><p>DONATE ₿</p></strong>
           <p><strong>♛ BTC:</strong> bc1qp4zxxj0k0whkm483qv5dk2rs6sgh2tt70h44rf</p>
           <p><strong>♛ LTC:</strong> ltc1qtyqsh3sgnaa9sdhe89p6qu2s6g75s70yzeqdyu</p>
       </p>
